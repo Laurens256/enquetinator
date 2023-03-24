1457c6b34e084a46b52252faad4bd6a99bf0e59e
Laurens256 <duinlaurens2@gmail.com>
2023-03-24 16:35:50 +0100

    initial form stuff

--- routes/home.ts
+++ routes/home.ts
@@ -7,17 +7,59 @@ interface IFormData {
 	email: string;
 }
 
+let formFields = [
+	{
+		type: 'text',
+		name: 'name',
+		label: 'Naam',
+		autocomplete: 'name',
+		classes: ['form-control'],
+		required: true,
+		value: '',
+		error: ''
+	},
+	{
+		type: 'number',
+		name: 'studentnumber',
+		label: 'Studentnummer',
+		autocomplete: 'off',
+		classes: ['form-control', 'nog-een-class'],
+		required: true,
+		value: '',
+		error: ''
+	},
+	{
+		type: 'email',
+		name: 'email',
+		label: 'Email',
+		autocomplete: 'email',
+		classes: ['form-control'],
+		required: true,
+		value: '',
+		error: ''
+	}
+];
+
 router.post('/', async (req, res) => {
 	const formData: IFormData = req.body;
 	const errors = validateForm(formData);
 
-	console.log(formData);
-
 	if (errors.name || errors.studentnumber || errors.email) {
+		// loop through formFields and set the value and error
+		formFields.forEach((field) => {
+			if (formData.hasOwnProperty(field.name)) {
+				field.value = formData[field.name as keyof IFormData];
+			}
+
+			if (errors.hasOwnProperty(field.name)) {
+				field.error = errors[field.name as keyof IFormData];
+			}
+		});
+
 		res.render('home', {
 			css: ['home'],
-			formData,
-			errors,
+			formFields,
+			errors
 		});
 	} else {
 		res.send('ok');
@@ -26,7 +68,8 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
 	res.render('home', {
-		css: ['home']
+		css: ['home'],
+		formFields
 	});
 });
 
@@ -36,27 +79,33 @@ const emailRegex =
 const studentNrRegex = /^[0-9]{9}$/;
 
 const validateForm = (formData: IFormData) => {
-	const errors: {
-		name?: string;
-		studentnumber?: string;
-		email?: string;
-	} = {};
+	const errors = {
+		name: '',
+		studentnumber: '',
+		email: ''
+	};
 	const { name, studentnumber, email } = formData;
 
 	if (name === '') {
 		errors.name = 'Naam is verplicht';
+	} else {
+		errors.name = '';
 	}
 
 	if (email === '') {
 		errors.email = 'Email is verplicht';
 	} else if (!emailRegex.test(formData.email)) {
 		errors.email = 'Email is niet geldig';
+	} else {
+		errors.email = '';
 	}
 
 	if (studentnumber === '') {
 		errors.studentnumber = 'Studentnummer is verplicht';
 	} else if (!studentNrRegex.test(formData.studentnumber)) {
 		errors.studentnumber = 'Studentnummer is niet geldig';
+	} else {
+		errors.studentnumber = '';
 	}
 
 	return errors;
